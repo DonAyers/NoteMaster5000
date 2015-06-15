93b78a42c13fcd53a46617dd0072f0246bb841f5
Don Ayers <donpaulayers@gmail.com>
2015-06-15 00:22:26 -0700

    update

--- handlers/update.js
+++ handlers/update.js
@@ -2,20 +2,8 @@
 var Note = require("../models/note");
 
 module.exports = function(req, res) {
-	var id = req.params.id;
-	var user = req.payload.user;
-	var content = req.payload.content;
-	var tag = req.payload.tag;
-	var color = req.payload.color;
-	console.log(user, color, tag, content, id);
-  var note = new Note({
-  	id:id,
-  	user: user,
-  	content: content,
-  	tag: tag,
-  	color: color
-  });
-  console.log(id, req.payload);
+  var note = new Note(req.payload);
+  console.log(req.payload);
   note.update(function() {
     var response = res("Note Updated");
     response.statusCode = 302;
